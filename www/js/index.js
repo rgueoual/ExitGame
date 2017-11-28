c88451e20613aa3e148c0f85dbbc251b35509d64
rgueoual <33941469+rgueoual@users.noreply.github.com>
2017-11-28 11:00:38 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,5 @@
-    var app = {
+nfc.addNdefListener(callback);
 
-    onNfc: function(nfcEvent) {
-        var tag = nfcEvent.tag;
-        app.display("Read tag: " + nfc.bytesToHexString(tag.id));
-   },
-
-/*
-   appends @message to the message div:
-*/
-   display: function(message) {
-      var label = document.createTextNode(message),
-         lineBreak = document.createElement("br");
-      messageDiv.appendChild(lineBreak);         // add a line break
-      messageDiv.appendChild(label);             // add the text
-   },
-/*
-   clears the message div:
-*/
-   clear: function() {
-       messageDiv.innerHTML = "";
-   },
-};     // end of app
-
-function win(){
-    alert("reading");
-};
-
-function fail(){
-    alert("failed to start reading");
-};
-
-function startReading(){
-    nfc.addTagDiscoveredListener(app.onNfc, win, fail);
-};
-
-
+function callback(){
+    alert("i just read something");
+}
